34059a3c9dda885656f9f61cd8c06d7ce6e0903d
joshgijo <200587199@student.georgianc.on.ca>
2024-08-08 19:15:18 -0400

    Add files via upload

---
+++ lab-5/script.js
@@ -0,0 +1,19 @@
+document.getElementById('global-header').innerHTML = `
+    <div class="logo">
+        <img src="images/sample-logo.png" alt="Logo">
+    </div>
+    <nav>
+        <a href="#">Home</a> |
+        <a href="#">About</a> |
+        <a href="#">Contact</a>
+    </nav>
+    <input type="text" placeholder="Search">
+`;
+
+document.getElementById('global-footer').innerHTML = `
+    <div class="footer-content">
+        <img src="images/sample-logo.png" alt="Footer Logo">
+        <p>Title</p>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam</p>
+    </div>
+`;
